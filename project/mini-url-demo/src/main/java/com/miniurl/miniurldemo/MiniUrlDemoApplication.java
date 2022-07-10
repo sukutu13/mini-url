23aa52b085f128459851a28f769b23fec2fdad8e
Leandro Miguel Silva <leandro13.silva13@hotmail.com>
2022-07-10 15:20:05 +0100

    Generation of the spring boot maven project

---
+++ project/mini-url-demo/src/main/java/com/miniurl/miniurldemo/MiniUrlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.miniurl.miniurldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUrlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniUrlDemoApplication.class, args);
+	}
+
+}
---
+++ project/mini-url-demo/src/test/java/com/miniurl/miniurldemo/MiniUrlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniurl.miniurldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniUrlDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
