1a40ad46797cecd8cebefb8381aec5042e9b7a79
Leandro Miguel Silva <leandro13.silva13@hotmail.com>
2022-07-12 19:29:30 +0100

    Creation of a constant file for erro messages

---
+++ project/mini-url-demo/src/main/java/com/miniurl/miniurldemo/constant/Constants.java
@@ -0,0 +1,23 @@
+package com.miniurl.miniurldemo.constant;
+
+public class Constants {
+
+    // #region CODES
+
+    public static String CODE_SUCESSO = "00";
+    public static String CODE_URL_NOT_FOUND = "01";
+    public static String CODE_INVALID_URL = "02";
+    public static String CODE_HASH_COLLISION = "03";
+
+    // #endregion
+
+    // #region DESCRIPTIONS
+
+    public static String DESCRIPTION_SUCESSO = "Sucesso.";
+    public static String DESCRIPTION_URL_NOT_FOUND = "Url não existe na base de dados.";
+    public static String DESCRIPTION_INVALID_URL = "Url Inválido.";
+    public static String DESCRIPTION_HASH_COLLISION = "Hash Collision.";
+
+    // #endregion
+
+}
